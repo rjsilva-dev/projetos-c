81f1f74f68dbd2cdb2de4503a5477ed8df823f12
Robson José <160296484+rjsilva-dev@users.noreply.github.com>
2024-03-19 05:39:49 -0300

    calculos usando switch
    codigo usando sinais

---
+++ calculos switch 2.c
@@ -0,0 +1,35 @@
+int main(){
+
+int num1,num2,total;
+char menu;
+
+printf("escolha uma operacao\n");
+printf("'+', '-', '*', '/' \n");
+scanf("%c",&menu);
+
+printf("primeiro valor: ");
+scanf("%d", &num1);
+printf("segundo valor: ");
+scanf("%d", &num2);
+
+switch (menu){
+case '+':
+    total = num1+num2;
+    printf("resultado da soma: %d",total);
+    break;
+case '-':
+    total = num1-num2;
+    printf("resultado da subtracao: %d",total);
+    break;
+case '*':
+    total = num1*num2;
+    printf("resultado da multiplicacao: %d",total);
+    break;
+case '/':
+    total = num1/num2;
+    printf("resultado da divisao: %d",total);
+    break;
+default:
+printf("opcao invalida");
+}
+}
